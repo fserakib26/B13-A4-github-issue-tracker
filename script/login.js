b6d403b4f52076523a11d5908483a7838fabaa90
fserakib26 <fserakib26@gmail.com>
2026-03-08 13:55:35 +0600

    Login Page Design and Function Completed using DaisyUI and Tailwind with JavaScript

---
+++ script/login.js
@@ -0,0 +1,23 @@
+
+
+document.getElementById("loginbtn").addEventListener("click", function () {
+    // console.log("Login Button has been clicked")
+    const userNameInput = document.getElementById("username");
+    const userName = userNameInput.value;
+    // console.log(userName);
+
+    const passwordInput = document.getElementById("password");
+    const userPassword = passwordInput.value;
+    // console.log(userPassword)
+
+    if(userName === "admin" && userPassword === "admin123"){
+        window.location.assign("/home.html")
+    } else {
+        alert('Login Failed! Type the Correct Username & Password')
+        return;
+    }
+});
+
+
+
+
