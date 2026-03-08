739346f0a5beec22288795f708471adda23ad977
fserakib26 <fserakib26@gmail.com>
2026-03-09 05:21:47 +0600

    Issue Card Dinamically shown from API

--- script/script.js
+++ script/script.js
@@ -6,20 +6,79 @@ const allIssues = () =>{
 };
 
 const displayAllIssues=(issues)=>{
-    // console.log(issues);
+    // console.log(issues); 
 
     const allIssuesContainer = document.getElementById('all-issues-container');
     allIssuesContainer.innerHTML = "";
 
 
     for(let issue of issues){
+        // Issue Counter
+        const issueCount = document.getElementById("issue-count");
+        issueCount.innerHTML = issue.id + " Issues";
+
+        let innerLabels = "";
+        for (let label of issue.labels){
+            innerLabels += `<span class="bg-yellow-100 rounded-full border border-yellow-500 text-yellow-600 px-2 py-1 mr-2 text-[10px]">
+                                ${label.toUpperCase()}
+                            </span>`;
+        }
+
         const issueCard = document.createElement('div')
         issueCard.innerHTML = `
-            
+            <div class="
+                ${issue.status === "open"?  "border-t-6 border-green-600 rounded-xl mt-6 shadow-xl" 
+                    : "border-t-6 border-purple-600 rounded-xl mt-6 shadow-xl" 
+                } h-full                
+                ">
+                <div class="pt-5 pl-5 pr-5">                
+                    <div class="space-y-5">
+                        <div class="flex items-center justify-between">
+                            <div>
+                                ${issue.status === "open"? `<img src="/assets/Open-Status.png" alt="">`: `<img src="/assets/Closed- Status .png" alt="">`}
+                            </div>
+
+                            <div>
+                                <span class="
+                                    ${issue.priority === "high"?  "bg-red-100 rounded-full text-red-500 px-3 py-1" :
+                                        issue.priority === "medium" ? "bg-yellow-100 rounded-full text-yellow-500 px-3 py-1" : 
+                                        "bg-slate-100 rounded-full text-slate-500 px-3 py-1"
+                                    }                               
+                                ">${issue.priority}</span>
+                            </div>
+                        </div>
+                        <div>
+                            <h2 class="text-xl font-bold mb-4">${issue.title}</h2>
+                            <p class=" text-slate-500">${issue.description}</p>
+                        </div>
+                        <div>
+                            <div>                                
+                                ${innerLabels}
+                            </div>                        
+                        </div>
+                    </div>                                
+                </div>
+
+                <div class="divider"></div>
+
+                <div class="pb-5 pl-5 pr-5 rounded-xl text-slate-500">
+                    <div>
+                        <span class="font-bold">${issue.author}</span>                        
+                    </div>
+                    <div>
+                        <span>${issue.createdAt}createdAt</span>
+                    </div>
+                </div>
+            </div>
         `;
         allIssuesContainer.appendChild(issueCard);
     }
     
 };
 
+
+
+
+
 allIssues();
+
