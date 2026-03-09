e47da37494b7652a9bfb5e926950ac2f449969cc
fserakib26 <fserakib26@gmail.com>
2026-03-09 15:57:11 +0600

    Sear Function Implemented by learn form ph and concept clear from google and gpt

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 // Global Variable Decleration
 let allIssuesData = [];
 
-const allIssues = () =>{
+const allIssues = () =>{    
     const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
     fetch(url)
         .then((res)=>res.json())
@@ -11,6 +11,8 @@ const allIssues = () =>{
 
             // Display Function called to show all data  
             displayAllIssues(allIssuesData); 
+            activeButton("all-btn");
+
             });    
 };
 
@@ -98,10 +100,10 @@ const openIssues = () =>{
 }
 
 // Closed Status Filtering for Closed Button
-const closedIssues = () =>{
+const closedIssues = () =>{    
     const closedData = allIssuesData.filter(issue => issue.status === "closed");
     displayAllIssues(closedData);
-    activeButton("closed-btn");
+    activeButton("closed-btn");    
 }
 
 // For All Button 
@@ -126,7 +128,24 @@ function activeButton(id){
 
 }
 
+// For Search
 
+const search = () => {
+    const searchText = document.getElementById("search-field").value.toLowerCase();
+    const findSearch = allIssuesData.filter(issue =>
+        issue.title.toLowerCase().includes(searchText) ||
+        issue.description.toLowerCase().includes(searchText) ||
+        issue.author.toLowerCase().includes(searchText) ||
+        issue.priority.toLowerCase().includes(searchText) ||
+        issue.status.toLowerCase().includes(searchText) ||
+        issue.labels.join(" ").toLowerCase().includes(searchText)
+    );
+    displayAllIssues(findSearch);
+};
+
+// Spinner Function
+const showSpinner = () => document.getElementById("spinner").classList.remove("hidden");
+const hideSpinner = () => document.getElementById("spinner").classList.add("hidden");
 
 
 
